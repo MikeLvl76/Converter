fc41d132883fbd080fb11a513dbc036540e21c61
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-11 20:37:24 +0200

    added comments and checks

--- converter.py
+++ converter.py
@@ -1,11 +1,19 @@
 class Converter:
 
+    '''
+    Dict for units, each key contains an array going from 10^-3 to 10^3.
+    Currently, there are three units : meter, liter and gram.
+    '''
     units = {
         'meter': ['mm', 'cm', 'dm', 'm', 'dam', 'hm', 'km'],
         'liter': ['ml', 'cl', 'dl', 'l', 'dal', 'hl', 'kl'],
         'gram': ['mg', 'cg', 'dg', 'g', 'dag', 'hg', 'kg']
     }
 
+    '''
+    Dict for conversion between two units.
+    Lambda function is better for easy and fast use.
+    '''
     conversion = {
         'gram-liter' : lambda x: x / 1000,
         'liter-gram' : lambda x: x * 1000,
@@ -13,6 +21,10 @@ class Converter:
         'meter-gram' : lambda x: x * 1000,
     }
 
+    '''
+    By one measure we get associated unit.
+    If not, None is returned.
+    '''
     def get_unit(self, measure = str):
         keys = self.units.keys()
         for key in keys:
@@ -20,6 +32,10 @@ class Converter:
                 return key
         return None
 
+    '''
+    The same goes for it but with conversion.
+    If not, None is returned
+    '''
     def get_conversion(self, measures = str):
         keys = self.conversion.keys()
         for key in keys:
@@ -27,6 +43,10 @@ class Converter:
                 return self.conversion[key]
         return None
 
+    '''
+    Get indices from measures and if they are different, conversion is taken.
+    All is returned as tuple.
+    '''
     def __make(self, arg = {}):
         unit_key = self.get_unit(arg['unit'])
         to_key = self.get_unit(arg['to'])
@@ -37,8 +57,14 @@ class Converter:
         print(f"Input : {arg['value']}{arg['unit']}")
         return (unit_index, to_index, scale)
 
+    '''
+    Make the conversion happen according to different case.
+    An empty text is returned if nothing happen.
+    '''
     def convert(self, arg = {}):
         text = ''
+        if arg['value'] in ['', None] or arg['unit'] in ['', None] or arg['to'] in ['', None] :
+            return text
         tools = self.__make(arg)
         if(tools[2] is not None):
             func = tools[2]
@@ -59,4 +85,4 @@ class Converter:
 
 
 converter = Converter()
-print(converter.convert({'value': 1, 'unit': 'kg', 'to': 'l'}))
+print(converter.convert({'value': 1, 'unit': 'kg', 'to': None}))
