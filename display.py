eeab25741c46314c6fdd3b4ac44cf7a294eb7050
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-14 10:17:44 +0200

    added widgets and bindings

--- display.py
+++ display.py
@@ -1,6 +1,7 @@
+from tkinter.ttk import Combobox
 from converters.classic_converter import ClassicConverter
 from converters.temperature_converter import TemperatureConverter
-from tkinter import Canvas, Tk
+from tkinter import Canvas, Tk, StringVar, Entry
 
 root = Tk()
 width = root.winfo_screenwidth()
@@ -10,5 +11,22 @@ root.geometry('x'.join([str(width), str(height)]))
 
 canvas = Canvas(root, width=width, height=height, background='gray')
 canvas.pack()
+
+value = StringVar()
+entry = Entry(canvas, textvariable=value)
+entry.pack()
+
+units = list(ClassicConverter.units.values())
+unit = []
+for sub in units:
+    for elt in sub:
+        unit.append(elt)
+
+comboboxClassic = Combobox(canvas, values=unit)
+comboboxClassic.pack()
+
+entry.event_add('<<end_input>>', '<Return>')
+entry.bind('<<end_input>>', lambda event : print(f"Written text !"))
+comboboxClassic.bind("<<ComboboxSelected>>", lambda event : print(f"An element is selected !"))
 
 root.mainloop()
