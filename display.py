cd9d080e46ea55e1518493f1a948fb56acf818f0
Mickael Louvel <mickael.louvel@etu.univ-lehavre.fr>
2022-07-18 13:56:21 +0200

    moved Tools class into new file

--- display.py
+++ display.py
@@ -1,100 +1,10 @@
-import re
 from tkinter import EW, NS, Text, ttk
-from tkinter.ttk import Combobox
 from converters.classic_converter import ClassicConverter
 from converters.temperature_converter import TemperatureConverter
 from converters.currency_converter import CurrencyConverter
-from tkinter import END, Button, Canvas, Label, Tk, StringVar, Entry, messagebox
+from tkinter import END, Label, messagebox
+from tool import Tools
 from storage.db_storage import DECODER, DBManager
-
-class Tools:
-
-    def __init__(self) -> None:
-        self.root = None
-        self.dimensions = ()
-        self.save = {}
-        self.result = ''
-
-    def create_window(self, title, dimensions = ()):
-        self.root = Tk()
-        self.root.title(title)
-        if len(dimensions) == 2:
-            width = dimensions[0]
-            height = dimensions[1]
-        else:
-            width = int(self.root.winfo_screenwidth() / 4)
-            height = int(self.root.winfo_screenheight() / 4)
-        self.dimensions = (width, height)
-        self.root.geometry('x'.join((str(width), str(height))))
-    
-    def create_canvas(self, master, dimensions = (), **args):
-        canvas = None
-        if len(dimensions) == 2:
-            canvas = Canvas(master, width=dimensions[0], height=dimensions[1], **args)
-        else:
-            canvas = Canvas(master, width=self.dimensions[0], height=self.dimensions[1], **args)
-        canvas.pack()
-        return canvas
-
-    def add_stringvar(self):
-        return StringVar()
-
-    def add_entry(self, master, string_var, **args):
-        entry = Entry(master, textvariable=string_var, **args)
-        entry.pack()
-        return entry
-
-    def add_button(self, master, **args):
-        button = Button(master, **args)
-        button.pack()
-        return button
-
-    def add_combobox(self, master, values = [], **args):
-        combobox = Combobox(master, values=values, **args)
-        combobox.pack()
-        return combobox
-
-    def add_label(self, master, **args):
-        label = Label(master, **args)
-        label.pack()
-        return label
-
-    def add_frame(self, master, **options):
-        return ttk.Frame(master, **options)
-
-    def add_notebook(self, master, **options):
-        return ttk.Notebook(master, **options)
-
-    def place(self, arg={}):
-        for key in arg.keys():
-            value = arg[key]
-            value[0].place(x=value[1], y=value[2], width=value[3], height=value[4])
-
-    def bind(self, item, type, callback):
-        item.bind(type, callback)
-
-    def change_state(self, item, state):
-        item['state'] = state
-
-    def loop(self):
-        self.root.mainloop()
-
-    def validate(self, regex, text):
-        if not re.match(regex, text):
-            messagebox.showerror('Error', f'Text "{text}" does not match expected input')
-        else:
-            print(f"Written text : {text}")
-
-    def saveInputs(self, *values):
-        self.save = {
-            'value': values[0],
-            'unit': values[1],
-            'to': values[2]
-        }
-
-    def save_result(self, converter, values):
-        self.result = converter.convert(values)
-        print(f'{self.result}')
 
 def switch_selected_values(c1, c2):
     if c1.get() != '' and c2.get() != '':
